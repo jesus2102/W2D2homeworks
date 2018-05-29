46263fc5782c122f59c16efbb16170ca3334304e
jesus2102 <jesusperezmarin@gmail.com>
2018-05-29 17:07:17 +0100

    Bear created

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -1,1 +1,14 @@
+require("minitest/autorun")
+require_relative("../bear.rb")
+require_relative("../river.rb")
+require_relative("../fish.rb")
+require("minitest/rg")
 
+
+class BearTest < MiniTest::Test
+
+  def setup
+    @bear = Bear.new("Yogi", "Grizzly")
+  end
+
+end
